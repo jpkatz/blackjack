1f75215ddb46053b26cdf3e9dce03ebcbe41c00c
jpkatz <46250338+jpkatz@users.noreply.github.com>
2019-06-07 20:13:24 -0400

    Added simulation class

--- blackjack.py
+++ blackjack.py
@@ -4,7 +4,8 @@ import random
 class Deck:
     
     def __init__(self):
-        cardNumber = 'A23456789TJQK' #All the numbers
+        #cardNumber = 'A23456789TJQK' #All the numbers
+        cardNumber = 'ATJQK'
         cardSuit = 'HDSC' #All the suits
         self.deck = {i+j for i in cardNumber for j in cardSuit} #get combinations
         
@@ -21,11 +22,9 @@ class Players(Deck):
     def handTotal(self):
         sum = 0
         hasAce = False
-        if 'A' in self.hand:
-            hasAce = True
         for i in self.hand:
             if i[0] in 'TJQK': sum +=10
-            elif i[0] == 'A': sum = sum
+            elif i[0] == 'A': sum = sum;hasAce = True
             else:             sum += int(i[0])
         if hasAce:
             sum = self.manageAces(sum)
@@ -34,15 +33,16 @@ class Players(Deck):
     def manageAces(self,sum):
         numberOfAces = 0
         for i in self.hand:
-            if self.hand[i][0]=='A':
+            if i[0]=='A':
                 numberOfAces+=1
         difference = 21 - sum
         
-        if difference <= 11:
+        if difference < 11:
             sum += numberOfAces
         else:
             sum += (numberOfAces-1) + 11
         return sum
+        
         
 class Dealer(Players):
     def __init__(self):
@@ -63,29 +63,71 @@ class Dealer(Players):
         return super().handTotal()
 
 class Player(Players):
-    def __init__(self):
+    def __init__(self,whereToStop):
         super().__init__()
+        self.whereToStop = whereToStop
         
-    def shouldHit(self):
-        if not self.hand :
-            return True
-        if self.handTotal()<20:
-            return True
-        else:
-            return False
-        
-        
-if __name__ == '__main__':
-    #initialize dealer and current player
-    dealer = Dealer()
-    me = Player()
-
-    #initialize 2 cards for players and dealer
-    for i in range(2):
-        dealer.hit()
-        me.hit()
+    def play(self):
+        notBust = True
+        stopHitting = False
+        while notBust and not stopHitting:
+            if super().handTotal() >= self.whereToStop:
+                stopHitting = True
+            else:
+                self.hit()
+            if super().handTotal() > 21:
+                notBust = False
+        return super().handTotal()
     
-    #checking if doing comp study or playing
+class Simulation():
+    def __init__(self,isSimulation,whereToStop):
+        self.isSimulation = isSimulation
+        self.whereToStop = whereToStop
+        
+    def newHand(self):
+        self.dealer = Dealer()
+        self.me = Player(self.whereToStop)
+        for i in range(2):
+            self.dealer.hit()
+            self.me.hit()
+            
+    def playGame(self):
+        if self.isSimulation:
+            playerHandCount = self.me.play()
+            if playerHandCount >21:
+                return False 
+            dealerHandCount = self.dealer.play()
+            if dealerHandCount>21 or playerHandCount >= dealerHandCount:
+                return True
+            else:
+                return False
+        elif not self.isSimulation:
+            notBust = True
+            playerStops = False
+            print('The card you can see is: ',self.dealer.hand[1])
+            while notBust and not playerStops:
+                print('Your current hand is ',self.me.hand,'\nWith total value: ',self.me.handTotal())
+                playerHit = input('Would you like to hit? ')
+                if playerHit != 'True' and playerHit != 'False':
+                    print('Please provide a boolean: True or False')
+                playerHit = bool(playerHit =='True')
+                if playerHit:
+                    self.me.hit()
+                else:
+                    playerStops = True
+                if self.me.handTotal()>21:
+                    notBust = False
+                    print('Sorry but you went bust: ',self.me.hand)
+            if notBust:
+                 dealerFinalHandCount = self.dealer.play()
+                 if dealerFinalHandCount >21 or dealerFinalHandCount <= self.me.handTotal():
+                     print('You Win!')
+                 else:
+                     print('You Lose!')
+
+                
+                
+if __name__ == '__main__':
     checkFormat = True
     while checkFormat:
         isSimulation = input('Is this a simulation? ')
@@ -94,31 +136,18 @@ if __name__ == '__main__':
         else:
             checkFormat = False
             isSimulation = bool(isSimulation=='True')
-    
-    #for game loop
-    notBust = True 
-    playerStops = False     
-    if not isSimulation:
-        print('The card you can see is: ',dealer.hand[1])
-        while notBust and not playerStops:
-            print('Your current hand is ',me.hand,'\nWith total value: ',me.handTotal())
-            playerHit = input('Would you like to hit? ')
-            if playerHit != 'True' and playerHit != 'False':
-                print('Please provide a boolean: True or False')
-            playerHit = bool(playerHit =='True')
-            if playerHit:
-                me.hit()
-            else:
-                playerStops = True
-            if me.handTotal()>21:
-                notBust = False
-                print('Sorry but you went bust: ',me.hand)
-        if notBust:
-            print('The dealer hand is: ',dealer.hand)
-            dealerFinalHandCount = dealer.play()
-            if dealerFinalHandCount >21 or dealerFinalHandCount < me.handTotal():
-                print('You Win!')
-            else:
-                print('You Lose!')
             
+    simulation = Simulation(isSimulation,17)
+    keepPlaying = True
+    while keepPlaying:
+        simulation.newHand()
+        simulation.playGame()
+        keepPlaying = input('Keep playing?')
+        checkFormat = True
+        while checkFormat:
+            if (keepPlaying!='True') and (keepPlaying != 'False'):
+                print('Please provide a boolean: True or False')
+            else:
+                checkFormat = False
+                keepPlaying = bool(keepPlaying=='True')
 
